68109d1ec8b5abb5bcbf9008f5a1a20e398aa00e
charon1212 <charons2turquoise@gmail.com>
2022-09-21 20:28:14 +0900

    add sample component for test.

---
+++ src/components/sample/Sample.tsx
@@ -0,0 +1,10 @@
+import { Button, Typography } from '@mui/material';
+
+export const Sample = () => {
+  return (
+    <>
+      <Typography>Sample Component</Typography>
+      <Button onClick={() => alert('sample')}>SAMPLE</Button>
+    </>
+  );
+};
