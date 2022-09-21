4f5afc20c309de5c0b3ee304fb2b137a43bd132f
charon1212 <charons2turquoise@gmail.com>
2022-09-21 22:33:13 +0900

    version 1.1.0

---
+++ src/components/Table/SimpleTable.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+import { TableContainer, Table, TableRow, TableCell, TableHead, TableBody, TableContainerProps, TableProps } from '@mui/material';
+
+type ColumnConfig<T extends object> = {
+  header: ReactNode;
+  cell: (obj: T) => ReactNode;
+};
+type Props<T extends object> = {
+  data: T[];
+  columns: ColumnConfig<T>[];
+  tableContainerProp: TableContainerProps;
+  tableProp: TableProps;
+};
+export const SimpleTable = <T extends object>(props: Props<T>) => {
+  const { data, columns, tableContainerProp, tableProp } = props;
+
+  return (
+    <>
+      <TableContainer {...tableContainerProp}>
+        <Table {...tableProp}>
+          <TableHead>
+            <TableRow>
+              {columns.map(({ header }, i) => (
+                <TableCell key={i}>{header}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((obj, i) => (
+              <TableRow key={i}>
+                {columns.map(({ cell }, i) => (
+                  <TableCell key={i}>{cell(obj)}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/sample/Sample';
 export * from './components/Tabs/TabComponent';
+export * from './components/Table/SimpleTable';
 
 export * from './hooks/useTextField';
