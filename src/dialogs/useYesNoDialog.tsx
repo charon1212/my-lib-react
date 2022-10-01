36ef5c8884aa8affd8350c1f9683f4a87edbc85b
charon1212 <charons2turquoise@gmail.com>
2022-10-02 08:20:24 +0900

    add YesNoDialog Context.

--- src/dialogs/useYesNoDialog.tsx
+++ src/dialogs/useYesNoDialog.tsx
@@ -1,20 +1,26 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 import { useState } from 'react';
 
-type YesNoDialogParam = {
+type YesNoDialogParam<Context extends any> = {
   title?: string;
   message: string;
-  onClickYes?: (closeDialog: () => void) => unknown;
-  onClickNo?: (closeDialog: () => void) => unknown;
+  onClickYes?: (closeDialog: () => void, context: Context) => unknown;
+  onClickNo?: (closeDialog: () => void, context: Context) => unknown;
+};
+type YesNoDialogReturn<Context extends any> = {
+  openYesNoDialog: [Context] extends [never] ? () => void : (context: Context) => void;
+  yesNoDialog: JSX.Element;
 };
 
 /**
  * Yes/Noダイアログ
  */
-export const useYesNoDialog = (param: YesNoDialogParam) => {
+export const useYesNoDialog = <Context extends any = never>(param: YesNoDialogParam<Context>): YesNoDialogReturn<Context> => {
   const { title, message, onClickYes, onClickNo } = param;
   const [open, setOpen] = useState(false);
-  const openYesNoDialog = () => {
+  const [context, setContext] = useState<Context>();
+  const openYesNoDialog = (context: Context) => {
+    setContext(context);
     setOpen(true);
   };
   const closeYesNoDialog = () => {
@@ -26,7 +32,7 @@ export const useYesNoDialog = (param: YesNoDialogParam) => {
       <Dialog
         open={open}
         onClose={() => {
-          if (onClickNo) onClickNo(closeYesNoDialog);
+          if (onClickNo) onClickNo(closeYesNoDialog, context!);
         }}
       >
         {title ? <DialogTitle>{title}</DialogTitle> : ''}
@@ -34,14 +40,14 @@ export const useYesNoDialog = (param: YesNoDialogParam) => {
         <DialogActions>
           <Button
             onClick={() => {
-              if (onClickYes) onClickYes(closeYesNoDialog);
+              if (onClickYes) onClickYes(closeYesNoDialog, context!);
             }}
           >
             Yes
           </Button>
           <Button
             onClick={() => {
-              if (onClickNo) onClickNo(closeYesNoDialog);
+              if (onClickNo) onClickNo(closeYesNoDialog, context!);
             }}
           >
             No
@@ -51,5 +57,5 @@ export const useYesNoDialog = (param: YesNoDialogParam) => {
     </>
   );
 
-  return { openYesNoDialog, yesNoDialog };
+  return { openYesNoDialog, yesNoDialog } as YesNoDialogReturn<Context>;
 };
