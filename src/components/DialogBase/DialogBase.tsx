2c3c6468e0d0ca1b57f291c0912831db347ffc0c
charon1212 <charons2turquoise@gmail.com>
2022-12-17 09:05:41 +0900

    version 1.7.1

--- src/components/DialogBase/DialogBase.tsx
+++ src/components/DialogBase/DialogBase.tsx
@@ -1,4 +1,5 @@
-import { Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { Dialog, DialogActions, DialogContent, DialogTitle, SxProps } from '@mui/material';
+import { Theme } from '@mui/system';
 
 type Props = {
   title?: React.ReactNode;
@@ -6,15 +7,21 @@ type Props = {
   actions?: React.ReactNode;
   open: boolean;
   onClose?: () => void;
+  sx?: {
+    dialog?: SxProps<Theme>;
+    dialogTitle?: SxProps<Theme>;
+    dialogContent?: SxProps<Theme>;
+    dialogActions?: SxProps<Theme>;
+  };
 };
 export const DialogBase = (props: Props) => {
-  const { title, content, actions, open, onClose } = props;
+  const { title, content, actions, open, onClose, sx } = props;
   return (
     <>
-      <Dialog open={open} onClose={onClose}>
-        {title ? <DialogTitle>{title}</DialogTitle> : ''}
-        {content ? <DialogContent>{content}</DialogContent> : ''}
-        {actions ? <DialogActions>{actions}</DialogActions> : ''}
+      <Dialog open={open} onClose={onClose} sx={sx?.dialog}>
+        {title ? <DialogTitle sx={sx?.dialogTitle}>{title}</DialogTitle> : ''}
+        {content ? <DialogContent sx={sx?.dialogContent}>{content}</DialogContent> : ''}
+        {actions ? <DialogActions sx={sx?.dialogActions}>{actions}</DialogActions> : ''}
       </Dialog>
     </>
   );
